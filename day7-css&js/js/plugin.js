5e66f2ff52f8384ac0e85b390081d90dff8fbb05
EngMaiBelal <EngMaiBelal>
2024-08-27 09:05:59 +0200

    first commit

---
+++ day7-css&js/js/plugin.js
@@ -0,0 +1,148 @@
+// variables
+// x = 2;        //number
+// y = "omar";   // string
+// z= "5";    //string
+
+// name convention
+// studentName ="omar";
+// student = "ahmed";
+// Student= "ali";
+// console.log(5+3);
+
+// let x, y, z, f;    // declaration
+// x = 5;          // assignment -- initialization
+// y = "6";          // Statement 3
+// z = y + x;
+// console.log(z);
+// m= null;
+// console.log(f); //undefind
+// console.log(h); error not defind
+// console.log(m); //null
+// boolean => true, false
+// h = true;
+// ---------------------------------------------------------------------
+// operators
+// 1- arithmetic operator % **++ --
+
+// x = 2;
+// console.log(x);
+// // ++x //prefix
+// // x++ //postfix
+// console.log(++x);
+// y = 3;
+// ---------------------------------------------------------------------
+// 2- comparison operator > < >= <= == === != return true or false
+// x= 2;
+// y= "2";
+// console.log(x == y); // value
+// console.log(x === y); // value & d.type // stricky equality
+// console.log(x != y); // value
+// console.log("1" == 1); //true
+// console.log("1" >= 1); //true
+// console.log("3" > 1); //true
+// type coercion
+
+//---------------------------------------------------------------------
+// 3-assignment operators
+// x = 10;
+// x= x + 5;
+// x += 5;
+
+//---------------------------------------------------------------------
+// 3-logical operators  =>boolean 
+// true && true=>true
+// false || true=>true
+// ! not
+
+
+// ----------------------------------------
+// x= 2;
+// y= 5;
+
+// x= "ahmed";
+// console.log(x);
+// console.log(typeof(x));
+// ----------------------------------------
+// output way
+// 1- console.log(data);
+// 2- document.getElementById('main').innerHTML= "ddddd";
+// 3- window.print();
+// 4- window.alert(data);
+// 5- document.write(data)
+// --------------------------
+// conditional statment 
+// if, switch
+// loop statments
+// for while dowhile for in 
+// if(condition){
+//     // result
+// }
+// x= 15;
+// y = 10;
+
+// if(y > x){
+//     document.write('okay');
+//     z= 20;
+// }else{
+//     document.write('it is not true');
+//     z= null;
+// }
+
+// console.log(z);
+// // --------------------------
+// myfavFruit= 'ay ggggg';
+
+// if(myfavFruit == 'apple'){
+//     alert('apple');
+// }else if(myfavFruit == 'banana'){
+//     alert('banana');
+// }else{
+//     alert(myfavFruit);
+// }
+// --------------------------
+// (myfavFruit == 'apple') ? alert('apple') : alert('no'); // performance
+// if(myfavFruit == 'apple'){
+//     alert('apple');
+// }else{
+//     alert('no');
+// }
+// --------------------------
+// let x = 0;
+
+// switch (x) {
+//     case 0:
+//     text = "Off";
+//     break;
+//     case 1:
+//     text = "On";
+//     break;
+//     default:
+//     text= " no text"
+//     break;
+// }
+// console.log(text);
+// x = 3;
+
+// if(x == 3 || x == 4){
+//    console.log("true");
+// }
+
+// switch(x){
+//     case 3:
+//     case 4:
+//         console.log("true");
+//     break;  
+// }
+
+
+var x = 3;  // global variable- 
+let y = 3;  // scopping variable (function)
+const z = 3; 
+
+y = 5;
+
+console.log(y);
+
+// -----------------------------
+
+
